5f2de2b46a67bbf6335d9d639435be8f0318595b
GuidoCARP <gbassi@frba.utn.edu.ar>
2025-02-06 10:47:31 -0300

    .

--- cierre.py
+++ cierre.py
@@ -5,8 +5,8 @@ from config import tickers
 def ejecutarCierre():
     enviar_resumen_cierre(tickers)
     enviarAvisoDeCruce(tickers)
-    tweet_cierre=compilar_contenido_cierre(tickers)
-    crear_hilo(tweet_cierre,"CIERRE DE LA BOLSA HOY")
+    #tweet_cierre=compilar_contenido_cierre(tickers)
+    #crear_hilo(tweet_cierre,"CIERRE DE LA BOLSA HOY")
 
 if __name__ == "__main__":
     
